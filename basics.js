fef56f48da6933444f794965b4fb45e778bf5786
Mateusz_Walowski <mat.walowski@gmail.com>
2021-02-05 10:01:44 +0100

    create basic files

---
+++ basics.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("hello world");
+
+
+// y = 14;
+
+const bool = "true";
+const num = "41";
+const str = "Peter";
+const obj = {
+    cats: 2,
+    cars: 1
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2"
